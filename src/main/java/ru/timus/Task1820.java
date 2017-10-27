5d08c410ce1156dbe072220c68dd6327d5f040dc
llixli <igor.belykh@protonmail.com>
2017-10-27 17:44:56 +0300

    Task 1820: debug v0

---
+++ src/main/java/ru/timus/Task1820.java
@@ -0,0 +1,18 @@
+package ru.timus;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Page of task: http://acm.timus.ru/problem.aspx?space=1&num=1001
+ */
+public class Task1820 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int[] numberOfStaks = new int[2];
+    }
+
+
+}
