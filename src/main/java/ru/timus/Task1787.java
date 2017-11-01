3d51a3f89f3331ee45791f4020d891b255dbdc3f
llixli <igor.belykh@protonmail.com>
2017-11-01 19:06:40 +0300

    Task1787: debug, wrong answer 2 when testing

--- src/main/java/ru/timus/Task1787.java
+++ src/main/java/ru/timus/Task1787.java
@@ -2,7 +2,6 @@ package ru.timus;
 
 
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -10,6 +9,7 @@ import java.util.Scanner;
  */
 public class Task1787 {
     public static void main(String[] args) {
+        Task1787 task1787 = new Task1787();
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
 
@@ -24,37 +24,74 @@ public class Task1787 {
 
         if (taskCondition.length() == 2 && !taskData.equals("")  ) {
             String concatenatedRow = taskCondition + " " + taskData;
-            numberOfCarsInCrossroad = checkNumberOfCarsOnCrossroad(concatenatedRow);
+            numberOfCarsInCrossroad = task1787.checkNumberOfCarsOnCrossroad(concatenatedRow);
         }
 
         out.println(numberOfCarsInCrossroad);
         out.flush();
     }
 
-    private static int checkNumberOfCarsOnCrossroad(String concatenatedRow) {
+    int checkNumberOfCarsOnCrossroad(String concatenatedRow) {
         int numberOfCarsInCrossroad = 0;
         String[] resultStringArray = concatenatedRow.split(" ");
-
+        int[] resultIntArray = new int[resultStringArray.length];
 
         try {
-            int[] resultIntArray = Arrays.stream(resultStringArray).mapToInt(Integer::parseInt).toArray();
-            boolean taskConditionIsReliable = false;
-            boolean taskDataIsReliable = false;
-            
-            numberOfCarsInCrossroad = calculateNumberOfCars(resultIntArray);
+            for (int i = 0; i < resultStringArray.length; i++) {
+                resultIntArray[i] = Integer.parseInt(resultStringArray[i]);
+            }
+            if (resultIntArray[1] == resultIntArray.length - 2) {
+                boolean conditionAndDataIsRelevant = checkNumbersToAccessoryOfLimit(resultIntArray);
+                if (conditionAndDataIsRelevant) {
+                    numberOfCarsInCrossroad = calculateNumberOfCars(resultIntArray);
+                }
+            }
         }
         catch (NumberFormatException e) {
             return numberOfCarsInCrossroad;
         }
+
         return numberOfCarsInCrossroad;
+    }
+
+    private boolean checkNumbersToAccessoryOfLimit(int[] resultIntArray) {
+        boolean conditionIsRelevant = true;
+        for (int i = 0; i < 1; i++) {
+            if (resultIntArray[i] < 0 || resultIntArray[i] > 100) {
+                conditionIsRelevant = false;
+                break;
+            }
+        }
+        boolean dataIsRelevant = true;
+        for (int i = 2; i < resultIntArray.length; i++) {
+            if (resultIntArray[i] < 1 || resultIntArray[i] > 100) {
+                dataIsRelevant = false;
+                break;
+            }
+        }
+        if (conditionIsRelevant && dataIsRelevant) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     private static int calculateNumberOfCars(int[] resultIntArray) {
         int numberOfCarsOnCrossroad = 0;
 
-        numberOfCarsOnCrossroad
+        int summaryValueOfCars = 0;
+        for (int i = 2; i < resultIntArray.length; i++) {
+            summaryValueOfCars += resultIntArray[i];
+        }
 
+        numberOfCarsOnCrossroad = resultIntArray[0] * resultIntArray[1] - summaryValueOfCars;
+        if (numberOfCarsOnCrossroad < 0) {
+            numberOfCarsOnCrossroad = Math.abs(numberOfCarsOnCrossroad);
+        }
+        else {
+            numberOfCarsOnCrossroad = 0;
+        }
         return numberOfCarsOnCrossroad;
     }
-
 }
