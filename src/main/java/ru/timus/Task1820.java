514008453d0082d6e74eff4fb64575f3a983e418
llixli <igor.belykh@protonmail.com>
2017-11-07 16:46:25 +0300

    Task1820: debug

--- src/main/java/ru/timus/Task1820.java
+++ src/main/java/ru/timus/Task1820.java
@@ -8,40 +8,56 @@ import java.util.ArrayList;
 */
 public class Task1820 {
     public static void main(String args[]) {
-    Task1820 task1820 = new Task1820();
+        Task1820 task1820 = new Task1820();
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
         ArrayList<Integer> inputData = new ArrayList<>();
-        int timeToCookingSteak = 0;
-      
+        int timeToCookingSteaks = 0;
+
         while(in.hasNextInt()) {
-        inputData.add(in.nextInt());
+            inputData.add(in.nextInt());
         }
 
         if (inputData.size() == 2) {
-        boolean dataIsCorrect = false;
-        for (int i = 0; i < inputData.size(); i++ ) {
-        dataIsCorrect = task1820.checkValue(inputData.get(i));
-        if (!dataIsCorrect) {
-        break;
-        }
-        }
-        if (dataIsCorrect) {
-        timeToCookingSteak = inputData.get(0) * inputData.get(1) * 2;
-        }
+            boolean dataIsCorrect = false;
+            for (int i = 0; i < inputData.size(); i++ ) {
+                dataIsCorrect = task1820.checkValue(inputData.get(i));
+                if (!dataIsCorrect) {
+                    break;
+                }
+            }
+            if (dataIsCorrect) {
+                int numberOfSteaks = inputData.get(0);
+                int capacityOfPan = inputData.get(1);
+                timeToCookingSteaks = task1820.calculateTime(numberOfSteaks, capacityOfPan);
+            }
         }
 
-        out.println(timeToCookingSteak);
+        out.println(timeToCookingSteaks);
         out.flush();
-}
+    }
 
-boolean checkValue(int inputValue) {
-	if (inputValue >= 1 && inputValue <= 1000) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
+    private int calculateTime(int numberOfSteaks, int capacityOfPan) {
+
+        int timeToCookingSteaks = 0;
+        if (capacityOfPan < numberOfSteaks) {
+            if (numberOfSteaks % capacityOfPan == 0) {
+                timeToCookingSteaks = 2 * numberOfSteaks
+            }
+        }
+        else {
+            timeToCookingSteaks = 2;
+        }
+        return timeToCookingSteaks;
+    }
+
+    boolean checkValue(int inputValue) {
+        if (inputValue >= 1 && inputValue <= 1000) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 }
